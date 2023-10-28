36e7fce19d04d1a86748d48acbf77886c687794d
Saurabh Singh <113841259+saurabhje@users.noreply.github.com>
2023-10-29 00:10:18 +0530

    CONTACT LINKS: #2

--- pages/index.js
+++ pages/index.js
@@ -30,10 +30,10 @@ export default function Home({ allPostsData }) {
           When I'm not plumbing the depths of the tech universe, I'm captivated by the enigmatic world of quantum mechanics. The idea of <strong>web3</strong> and <strong>decentralization</strong> holds an irresistible allure for me, and I'm on the brink of a thrilling transition into this uncharted domain. My inaugural stride in this direction is the acquisition of Solidity, a language that unlocks a portal to a realm teeming with unexplored prospects.
         <p>
           In a universe often besieged by intricacy, I'm here to champion the ideals of <strong>simplicity</strong>, <strong>efficiency</strong>,<strong> scalable</strong> and the allure of intricate functionality. Join me on this journey, where we celebrate the craft of forging applications that are irresistibly captivating and artfully intricate.
-          You can contact me on <a href='https://twitter.com/saurabhjesingh'>Twitter</a>.
+          You can contact me on <a href='https://twitter.com/saurabhjesingh'>Twitter</a> and <a href='https://www.linkedin.com/in/saurabhje/'>LinkedIn</a> .
         </p>
         <p>
-          (This is a sample and minimalist blog - created using{' '}
+          (This is a simple and minimalist blog - created using{' '}
           <a href="https://nextjs.org/learn">NextJS</a> and
           <a href="https://www.sanity.io/"> Sanity </a>
           )
