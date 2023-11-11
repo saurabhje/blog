fe4e3ff5833f487b28d98ad8fed0e8f143cea2e5
saurabhje <saurabhjesingh@gmail.com>
2023-11-11 22:02:22 +0530

    Navlink

--- pages/about.js
+++ pages/about.js
@@ -10,16 +10,11 @@ export default function About() {
       </Head>
       <div className={utilStyles.headingMd}>
       <p>
-        Hey there! I'm <strong>Je</strong>, a software engineer who enjoys creating things. I'm all about crafting apps that are simple, efficient, and just plain awesome.
+        Hi, I'm <span style={{ fontSize: 'larger', fontWeight: 'bold' }}>Je</span>. I am never sleepless. I proudly go by <strong>megamind</strong>, though my intellect is more fetch-the-ball than solve-the-universe. And my emotional intelligence? Picture a potato having an existential crisis – that's me at my best!
       </p>
       <p>
-        I'm not a fan of flashy graphics or complicated websites. I prefer to keep things clean and minimal while ensuring they're enjoyable to use. Imagine it as making cool stuff without the unnecessary complexity.
-      </p>
-      <p>
-        When I'm not immersed in the tech world, I'm exploring the intriguing realm of quantum mechanics. I'm also thrilled about the concept of <strong>web3</strong> and <strong>decentralization</strong>. I'm on a journey to grasp the fundamentals of web3, which promises a world of exciting possibilities.
-      </p>
-      <p>
-        In a world filled with <i><strong>complexity</strong></i>, I prioritize <i><strong>simplicity</strong></i>, <i><strong>efficiency</strong></i> and <i>scalability</i> (lies,that is something I'm still learning). Join me on this adventure as we create fantastic apps that are user-friendly and clever. Feel free to reach out to me on <a href='https://twitter.com/saurabhjesingh'>Twitter</a> and <a href='https://www.linkedin.com/in/saurabhje/'>LinkedIn</a>. And don't worry, I'm not too serious – I'm always up for a good laugh. 😄
+        I am a red flag, not the one which girls like. I am the real one -
+        I listen to Kanye West.
       </p>
       </div>
     </Layout>
