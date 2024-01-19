560e87862513b7c1925f2b423151ac1935359c09
Saurabh Singh <113841259+saurabhje@users.noreply.github.com>
2024-01-19 21:49:14 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -24,14 +24,9 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>This is where I write stuff. Abandon all hope. Leave all uncertainty of my future. And dump all multimillion imaginary ideas. Pretty sick i must say for a delusion enthuasist</p>
+        <p>Hello there! My name is Saurabh Je Singh, and I'm a sophomore studying Machine Learning Engineering. Through this platform, I aim to share my experiences as an engineering student, the challenges I face, and how I overcome them. As an enthusiastic learner, I'm constantly seeking opportunities to enhance my knowledge and skills in machine learning and related fields, while exploring their practical applications in real-world scenarios.</p>
         <p>
-
-        </p>
-        <p>
-          This is a simple and minimalist blog - created using{' '}
-          <a href="https://nextjs.org/learn">NextJS</a> and
-          <a href="https://www.sanity.io/"> Sanity </a>
+          I invite you to explore my blog, where I share my thoughts on various topics related to machine learning, programming, and technology. I also post updates on my projects, which include experiments, simulations, and implementations of machine learning models and algorithms. Additionally, I share resources and tips that I find helpful in my own learning journey, such as tutorials, books, and online courses.
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px} ${utilStyles.margin}`}>
